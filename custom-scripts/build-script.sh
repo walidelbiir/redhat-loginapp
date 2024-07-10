63ee63a647fa83c83d824166480cdef97d5a812f
walidelbiir <walid.el.biir@gmail.com>
2024-07-10 17:38:45 +0100

    custom strategy

---
+++ custom-scripts/build-script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+# Access source code
+cd /tmp/src
+
+# Set up variables for the internal registry
+REGISTRY="image-registry.openshift-image-registry.svc:5000"
+NAMESPACE="loginapp-dev"
+IMAGE_STREAM_NAME="login-app-custom-strat"
+IMAGE_TAG="latest" 
+USERNAME="user"
+PASSWORD="user@1243"
+
+# Full image name
+FULL_IMAGE_NAME="${REGISTRY}/${NAMESPACE}/${IMAGE_STREAM_NAME}:${IMAGE_TAG}"
+
+# Build the image
+buildah bud -t ${FULL_IMAGE_NAME} .
+
+# Authenticate with the internal registry
+#TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+#buildah login -u openshift -p ${TOKEN} ${REGISTRY} --tls-verify=false
+
+# Connect to internal openshift registry
+buildah login -u ${USERNAME} -p ${PASSWORD} $REGISTRY --tls-verify=false
+
+# Push the image
+buildah push ${FULL_IMAGE_NAME} --tls-verify=false
+
+echo "Image pushed successfully to ${FULL_IMAGE_NAME}"
+
+
+
+
