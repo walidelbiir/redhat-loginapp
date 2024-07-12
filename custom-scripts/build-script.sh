b9bc5e19cec4febc6ccb7b64f34e6231d8f0f3cc
walidelbiir <walid.el.biir@gmail.com>
2024-07-12 14:06:08 +0100

    ok

--- custom-scripts/build-script.sh
+++ custom-scripts/build-script.sh
@@ -8,14 +8,14 @@ cd /tmp/src
 
 # Full image name
 FULL_IMAGE_NAME="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
 buildah login -u walid1243 -p BirBir123Bir123 registry.redhat.io
 
 # Build the image
 buildah bud -t ${FULL_IMAGE_NAME} -f ./Dockerfile-tomcat .
 
-cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
-(echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
+buildah login -u builder -p ${TOKEN} ${OUTPUT_REGISTRY}
 
 buildah push --tls-verify=false ${FULL_IMAGE_NAME} 
 
