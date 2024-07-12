0ede5c9944d26be32edc3d16655c140af18364b4
walidelbiir <walid.el.biir@gmail.com>
2024-07-12 14:20:48 +0100

    ok

--- custom-scripts/build-script.sh
+++ custom-scripts/build-script.sh
@@ -17,6 +17,9 @@ buildah bud -t ${FULL_IMAGE_NAME} -f ./Dockerfile-tomcat .
 
 buildah login -u builder -p ${TOKEN} ${OUTPUT_REGISTRY} --tls-verify=false
 
+cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
+(echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
+
 buildah push --tls-verify=false ${FULL_IMAGE_NAME} 
 
 
