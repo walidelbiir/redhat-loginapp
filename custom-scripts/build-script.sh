9cc825318301bbee05b9e962fec225ed493bae3e
walidelbiir <walid.el.biir@gmail.com>
2024-07-12 11:58:36 +0100

    test

--- custom-scripts/build-script.sh
+++ custom-scripts/build-script.sh
@@ -2,36 +2,18 @@
 
 set -e
 
-git clone $SOURCE_URI /tmp/src
+#git clone $SOURCE_URI /tmp/src
 
-cd /tmp/src
-
-# Set up variables for the internal registry
-REGISTRY="image-registry.openshift-image-registry.svc:5000"
-NAMESPACE="loginapp-dev"
-IMAGE_STREAM_NAME="login-app-custom-strat"
-IMAGE_TAG="latest" 
-USERNAME="user"
-PASSWORD="user@1243"
+#cd /tmp/src
 
 # Full image name
-FULL_IMAGE_NAME="${REGISTRY}/${NAMESPACE}/${IMAGE_STREAM_NAME}:${IMAGE_TAG}"
-buildah login -u walid1243 -p BirBir123Bir123 registry.redhat.io
-
+FULL_IMAGE_NAME="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 
 # Build the image
 buildah bud -t ${FULL_IMAGE_NAME} -f ./Dockerfile-tomcat .
 
 
-# Authenticate with the internal registry
-TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-buildah login -u custom-builder -p ${TOKEN} ${REGISTRY} --tls-verify=false
 
-
-# Push the image
-buildah push ${FULL_IMAGE_NAME} --tls-verify=false
-
-echo "Image pushed successfully to ${FULL_IMAGE_NAME}"
 
 
 
