5d7d7709aea827d879214ac5b6ab6282bac2a54a
walidelbiir <walid.el.biir@gmail.com>
2024-07-31 11:36:45 +0100

    cc

--- custom-scripts/build-script.sh
+++ custom-scripts/build-script.sh
@@ -21,10 +21,3 @@ cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
 (echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
 
 buildah push --tls-verify=false --authfile /tmp/.dockercfg ${FULL_IMAGE_NAME} 
-
-
-
-
-
-
-
