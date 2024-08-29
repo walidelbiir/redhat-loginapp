d31226c0ea50aa4f86302d76e12fcb84b2f33c25
walidelbiir <walid.el.biir@gmail.com>
2024-08-29 13:21:42 +0100

    test

--- custom-scripts/build-script.sh
+++ custom-scripts/build-script.sh
@@ -5,7 +5,7 @@ set -e
 git config --global user.name "walidelbir"
 git config --global user.email "walid.el.biir@gmail.com"
 
-gh auth login --with-token < <echo($PAT_KEY)
+gh auth login --with-token < <(echo $PAT_KEY)
 
 git clone $SOURCE_URI /tmp/src
 
